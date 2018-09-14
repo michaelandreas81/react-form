d253d644df9e3a499d7a058d5897b9d0dd6e07ec
michael andreas <hello@michaelandreas.co.uk>
2018-09-14 21:59:00 +0100

    Remove header and footer

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -18,21 +18,17 @@ import Footer from 'components/Footer';
 import './style.scss';
 
 const App = () => (
-  <div className="app-wrapper">
-    <Helmet
-      titleTemplate="%s - React.js Boilerplate"
-      defaultTitle="React.js Boilerplate"
-    >
-      <meta name="description" content="A React.js Boilerplate application" />
-    </Helmet>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/features" component={FeaturePage} />
-      <Route path="" component={NotFoundPage} />
-    </Switch>
-    <Footer />
-  </div>
+    <div className="app-wrapper">
+        <Helmet
+            titleTemplate="%s - React.js Boilerplate"
+            defaultTitle="React.js Boilerplate"
+        >
+        </Helmet>
+        <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="" component={NotFoundPage}/>
+        </Switch>
+    </div>
 );
 
 export default App;
