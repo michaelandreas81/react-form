8f7655b5a1cd59934c7a6b55416e8c800d9c0045
michael andreas <hello@michaelandreas.co.uk>
2018-09-15 19:20:55 +0100

    Add onClick prop

--- app/components/CustomButton/CustomButton.js
+++ app/components/CustomButton/CustomButton.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import './style.scss';
-
+import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
-const CustomButton = () => (
+
+const CustomButton = ({ onClick }) => (
     <Button
         bsSize="small"
         type="submit"
+        onClick={onClick}
     >
         Next >
     </Button>
 );
+
+CustomButton.propTypes = {
+    onClick: PropTypes.func.isRequired,
+};
 
 export default CustomButton;
