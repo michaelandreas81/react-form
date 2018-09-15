ca75e9eeb5da65dcedcb89a92557231f7dafd223
michael andreas <hello@michaelandreas.co.uk>
2018-09-15 23:57:42 +0100

    Add step 1 reducers

--- app/containers/HomePage/reducer.js
+++ app/containers/HomePage/reducer.js
@@ -11,21 +11,33 @@
  */
 import { fromJS } from 'immutable';
 
-import { CHANGE_USERNAME } from './constants';
+import { CHANGE_USERNAME, ACTIVE_PANEL, FIRST_NAME, SURNAME, EMAIL_ADDRESS } from './constants';
 
 // The initial state of the App
 const initialState = fromJS({
-  username: ''
+    username: '',
+    active: 1,
+    firstName: '',
+    surname: '',
+    emailAddress: '',
 });
 
 function homeReducer(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_USERNAME:
-      // Delete prefixed '@' from the github username
-      return state.set('username', action.name.replace(/@/gi, ''));
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case CHANGE_USERNAME:
+            // Delete prefixed '@' from the github username
+            return state.set('username', action.name.replace(/@/gi, ''));
+        case ACTIVE_PANEL:
+            return state.set('active', action.active);
+        case FIRST_NAME:
+            return state.set('firstName', action.firstName);
+        case SURNAME:
+            return state.set('surname', action.surname);
+        case EMAIL_ADDRESS:
+            return state.set('email', action.emailAddress);
+        default:
+            return state;
+    }
 }
 
 export default homeReducer;
