4d229a5a10f99e358c9a111feacdd8be1f0765f2
michael andreas <hello@michaelandreas.co.uk>
2018-09-15 15:03:42 +0100

    Create custom button component

---
+++ app/components/CustomButton/CustomButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+import { Button } from 'react-bootstrap';
+
+const CustomButton = () => (
+    <Button
+        bsSize="small"
+        type="submit"
+    >
+        Next >
+    </Button>
+);
+
+export default CustomButton;
