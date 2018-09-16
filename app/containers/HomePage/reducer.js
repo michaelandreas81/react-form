3ff4c84706acf732076942e812dc52c5ce505cb2
michael andreas <hello@michaelandreas.co.uk>
2018-09-16 09:52:21 +0100

    Create all reducers

--- app/containers/HomePage/reducer.js
+++ app/containers/HomePage/reducer.js
@@ -11,15 +11,33 @@
  */
 import { fromJS } from 'immutable';
 
-import { CHANGE_USERNAME, ACTIVE_PANEL, FIRST_NAME, SURNAME, EMAIL_ADDRESS } from './constants';
+import {
+    CHANGE_USERNAME,
+    ACTIVE_PANEL,
+    FIRST_NAME,
+    SURNAME,
+    EMAIL_ADDRESS,
+    TELEPHONE_NUMBER,
+    GENDER, DATE_OF_BIRTH, COMMENTS,
+} from './constants';
 
 // The initial state of the App
 const initialState = fromJS({
     username: '',
     active: 1,
-    firstName: '',
-    surname: '',
-    emailAddress: '',
+    step1: {
+        firstName: '',
+        surname: '',
+        emailAddress: '',
+    },
+    step2: {
+        telephoneNumber: '',
+        gender: '',
+        dateOfBirth: '',
+    },
+    step3: {
+        comments: ''
+    }
 });
 
 function homeReducer(state = initialState, action) {
@@ -30,11 +48,19 @@ function homeReducer(state = initialState, action) {
         case ACTIVE_PANEL:
             return state.set('active', action.active);
         case FIRST_NAME:
-            return state.set('firstName', action.firstName);
+            return state.setIn(['step1', 'firstName'], action.firstName);
         case SURNAME:
-            return state.set('surname', action.surname);
+            return state.setIn(['step1', 'surname'], action.surname);
         case EMAIL_ADDRESS:
-            return state.set('email', action.emailAddress);
+            return state.setIn(['step1', 'emailAddress'], action.emailAddress);
+        case TELEPHONE_NUMBER:
+            return state.setIn(['step2', 'telephoneNumber'], action.telephoneNumber);
+        case GENDER:
+            return state.setIn(['step2', 'gender'], action.gender);
+        case DATE_OF_BIRTH:
+            return state.setIn(['step2', 'dateOfBirth'], action.dateOfBirth);
+        case COMMENTS:
+            return state.setIn(['step3', 'comments'], action.comments);
         default:
             return state;
     }
