d88aaa6251ee5b1d3318b545a63328912dc9d5d4
michael andreas <hello@michaelandreas.co.uk>
2018-09-16 09:57:15 +0100

    Add all actions for date inputs

--- app/containers/HomePage/actions.js
+++ app/containers/HomePage/actions.js
@@ -15,7 +15,17 @@
  *    }
  */
 
-import { CHANGE_USERNAME } from './constants';
+import {
+    ACTIVE_PANEL,
+    CHANGE_USERNAME,
+    COMMENTS,
+    DATE_OF_BIRTH,
+    EMAIL_ADDRESS,
+    FIRST_NAME,
+    GENDER,
+    SURNAME,
+    TELEPHONE_NUMBER,
+} from './constants';
 
 /**
  * Changes the input field of the form
@@ -25,8 +35,120 @@ import { CHANGE_USERNAME } from './constants';
  * @return {object}    An action object with a type of CHANGE_USERNAME
  */
 export function changeUsername(name) {
-  return {
-    type: CHANGE_USERNAME,
-    name
-  };
+    return {
+        type: CHANGE_USERNAME,
+        name,
+    };
+}
+
+/**
+ * Changes Panel of the accordion
+ *
+ * @param  {active} active The active panel
+ *
+ * @return {number}    A action number with a type of ACTIVE_PANEL
+ */
+export function activePanel(active) {
+    return {
+        type: ACTIVE_PANEL,
+        active,
+    };
+}
+
+/**
+ * Changes the input field of the form
+ *
+ * @param  {firstName} firstName The new text of the input field
+ *
+ * @return {text}    A action text with a type of FIRST_NAME
+ */
+export function inputFirstName(firstName) {
+    return {
+        type: FIRST_NAME,
+        firstName,
+    };
+}
+
+/**
+ * Changes the input field of the form
+ *
+ * @param  {surname} surname The new text of the input field
+ *
+ * @return {text}    A action text with a type of FIRST_NAME
+ */
+export function inputSurname(surname) {
+    return {
+        type: SURNAME,
+        surname,
+    };
+}
+
+/**
+ * Changes the input field of the form
+ *
+ * @param  {emailAddress} emailAddress The new text of the input field
+ *
+ * @return {text}    A action text with a type of EMAIL_ADDRESS
+ */
+export function inputEmailAddress(emailAddress) {
+    return {
+        type: EMAIL_ADDRESS,
+        emailAddress,
+    };
+}
+
+/**
+ * Changes the input field of the form
+ *
+ * @param  {telephoneNumber} telephoneNumber The new text of the input field
+ *
+ * @return {number}    A action number with a type of TELEPHONE_NUMBER
+ */
+export function inputTelephoneNumber(telephoneNumber) {
+    return {
+        type: TELEPHONE_NUMBER,
+        telephoneNumber,
+    };
+}
+
+/**
+ * Changes the input field of the form
+ *
+ * @param  {gender} gender The new text of the input field
+ *
+ * @return {text}    A action text with a type of GENDER
+ */
+export function inputGender(gender) {
+    return {
+        type: GENDER,
+        gender,
+    };
+}
+
+/**
+ * Changes the input field of the form
+ *
+ * @param  {dateOfBirth} dateOfBirth The new text of the input field
+ *
+ * @return {number}    A action number with a type of DATE_OF_BIRTH
+ */
+export function inputDateOfBirth(dateOfBirth) {
+    return {
+        type: DATE_OF_BIRTH,
+        dateOfBirth,
+    };
+}
+
+/**
+ * Changes the input field of the form
+ *
+ * @param  {comments} comments The new text of the input field
+ *
+ * @return {text}    A action text with a type of COMMENTS
+ */
+export function inputComments(comments) {
+    return {
+        type: COMMENTS,
+        comments,
+    };
 }
