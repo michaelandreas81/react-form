922a5cb4d3ea31a5716b492e5f04567ddfa59c8f
michael andreas <hello@michaelandreas.co.uk>
2018-09-16 09:58:00 +0100

    Add all steps to selectors

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -7,11 +7,59 @@ import { createSelector } from 'reselect';
 const selectHome = (state) => state.get('home');
 
 const makeSelectUsername = () => createSelector(
-  selectHome,
-  (homeState) => homeState.get('username')
+    selectHome,
+    (homeState) => homeState.get('username'),
+);
+
+const makeSelectActivePanel = () => createSelector(
+    selectHome,
+    (homeState) => homeState.get('active'),
+);
+
+const makeSelectFirstName = () => createSelector(
+    selectHome,
+    (homeState) => homeState.getIn(['step1', 'firstName']),
+);
+
+const makeSelectSurname = () => createSelector(
+    selectHome,
+    (homeState) => homeState.getIn(['step1', 'surname']),
+);
+
+const makeSelectEmailAddress = () => createSelector(
+    selectHome,
+    (homeState) => homeState.getIn(['step1', 'emailAddress']),
+);
+
+const makeSelectTelephoneNumber = () => createSelector(
+    selectHome,
+    (homeState) => homeState.getIn(['step2', 'telephoneNumber']),
+);
+
+const makeSelectGender = () => createSelector(
+    selectHome,
+    (homeState) => homeState.getIn(['step2', 'gender']),
+);
+
+const makeSelectDateOfBirth = () => createSelector(
+    selectHome,
+    (homeState) => homeState.getIn(['step2', 'DateOfBirth']),
+);
+
+const makeSelectComments = () => createSelector(
+    selectHome,
+    (homeState) => homeState.getIn(['step3', 'comments']),
 );
 
 export {
-  selectHome,
-  makeSelectUsername,
+    selectHome,
+    makeSelectUsername,
+    makeSelectActivePanel,
+    makeSelectFirstName,
+    makeSelectSurname,
+    makeSelectEmailAddress,
+    makeSelectTelephoneNumber,
+    makeSelectGender,
+    makeSelectDateOfBirth,
+    makeSelectComments,
 };
