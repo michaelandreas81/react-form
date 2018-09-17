08742f423c62ae7dcc064ef6a95034fdcbb4baf6
michael andreas <hello@michaelandreas.co.uk>
2018-09-17 16:03:58 +0100

    Remove unused code

--- app/app.js
+++ app/app.js
@@ -35,11 +35,12 @@ import configureStore from './configureStore';
 const openSansObserver = new FontFaceObserver('Open Sans', {});
 
 // When Open Sans is loaded, add a font-family using Open Sans to the body
-openSansObserver.load().then(() => {
-  document.body.classList.add('fontLoaded');
-}, () => {
-  document.body.classList.remove('fontLoaded');
-});
+openSansObserver.load()
+    .then(() => {
+        document.body.classList.add('fontLoaded');
+    }, () => {
+        document.body.classList.remove('fontLoaded');
+    });
 
 // Create redux store with history
 const initialState = {};
@@ -48,26 +49,26 @@ const store = configureStore(initialState, history);
 const MOUNT_NODE = document.getElementById('app');
 
 const render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      {/* <LanguageProvider messages={messages}> */}
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-      {/* </LanguageProvider> */}
-    </Provider>,
-    MOUNT_NODE
-  );
+    ReactDOM.render(
+        <Provider store={store}>
+            {/* <LanguageProvider messages={messages}> */}
+            <ConnectedRouter history={history}>
+                <App/>
+            </ConnectedRouter>
+            {/* </LanguageProvider> */}
+        </Provider>,
+        MOUNT_NODE,
+    );
 };
 
 if (module.hot) {
-  // Hot reloadable React components and translation json files
-  // modules.hot.accept does not accept dynamic dependencies,
-  // have to be constants at compile-time
-  module.hot.accept(['containers/App'], () => {
-    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
-    render();
-  });
+    // Hot reloadable React components and translation json files
+    // modules.hot.accept does not accept dynamic dependencies,
+    // have to be constants at compile-time
+    module.hot.accept(['containers/App'], () => {
+        ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+        render();
+    });
 }
 
 render();
